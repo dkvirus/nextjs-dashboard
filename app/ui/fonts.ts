d3f4d7f22b11ae331be97aa6b2e27119f15466bb
dkvirus <me@dkvirus.com>
2024-01-07 11:02:11 +0800

    chapter5

---
+++ app/ui/fonts.ts
@@ -0,0 +1,5 @@
+import { Nunito_Sans, Lusitana } from 'next/font/google';
+
+export const nunitoSans = Nunito_Sans({ subsets: ['latin'] })
+
+export const lusitana = Lusitana({ subsets: ['latin'], weight: [ '400', '700' ] })
